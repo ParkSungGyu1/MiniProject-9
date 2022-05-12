2e17f2d73078aec3018d7fab5cc76f3dd7b004e2
ParkSungGyu1 <steven0507@naver.com>
2022-05-12 11:20:21 +0900

    메인 페이지 구현 완료.

--- app.py
+++ app.py
@@ -134,14 +134,18 @@ def info_post():
 @app.route("/info", methods=["GET"])
 def info_get():
     info_list = list(db.nullforyou.find({}, {'_id': False}))
-    return jsonify({'infos':info_list})
+    return jsonify({'msg':info_list})
 
 @app.route("/boxcheck", methods=["POST"])
 def boxcheck():
     checkval_receive = request.form['check_val']
     result = list(db.nullforyou.find({'title': {"$regex": checkval_receive}} , {'_id': False}))
-    print(result)
-    return jsonify({'msg': result})
+    if checkval_receive == "all":
+        info_list = list(db.nullforyou.find({}, {'_id': False}))
+        return jsonify({'msg': info_list})
+    else:
+        return jsonify({'msg': result})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
