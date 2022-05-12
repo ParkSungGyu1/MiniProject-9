a11fe38cbc45f3de59bbf029e827ba7060936098
ParkSungGyu1 <steven0507@naver.com>
2022-05-12 18:25:07 +0900

    jwt 삭제

--- app.py
+++ app.py
@@ -17,15 +17,7 @@ SECRET_KEY = 'SPARTA'
 
 @app.route('/')
 def home():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"id": payload["id"]})
-        return render_template('page_main.html', user_info=user_info)
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    return render_template('index.html')
 
 
 @app.route('/main')
@@ -50,13 +42,7 @@ def sign_in():
     result = db.users.find_one({'id': username_receive, 'password': pw_hash})
 
     if result is not None:
-        payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        return jsonify({'result': 'success', 'token': token})
+        return jsonify({'result': 'success'})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
